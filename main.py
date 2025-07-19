a9f5ec4673b2dc854fd036c8eb7e95feb1cc17ed
Lucas Garcia Marcos <lucas.marcos@careintelligence.ai>
2025-07-19 01:08:49 -0300

    Fix: ctx é vago, melhor context

--- main.py
+++ main.py
@@ -12,28 +12,27 @@ intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix='$', intents=intents)
 
-# URL exclusivo kasino
-KASINO = "https://youtu.be/LCDaw0QmQQc?si=s9E1Z9QNThLdL9qc"
-
 @bot.event
 async def on_ready():
     print(f'{bot.user} está online')
 
 @bot.command(name='kasino')
-async def kasino(ctx):
-    await play(ctx, KASINO)
-    await ctx.send("SABADASSO")
+async def kasino(context):
+    # URL exclusivo kasino
+    KASINO = "https://youtu.be/LCDaw0QmQQc?si=s9E1Z9QNThLdL9qc"
+    await play(context, KASINO)
+    await context.send("SABADASSO")
 
 @bot.command(name='play')
-async def play(ctx, url):
+async def play(context, url):
     # Verifica se o usuário está em um canal de voz
-    if not ctx.author.voice:
-        await ctx.send("Você precisa estar em um canal de voz")
+    if not context.author.voice:
+        await context.send("Você precisa estar em um canal de voz")
         return
     
     # Conecta ao canal de voz do usuário
-    channel = ctx.author.voice.channel
-    voice_client = ctx.voice_client
+    channel = context.author.voice.channel
+    voice_client = context.voice_client
     
     if voice_client is None:
         voice_client = await channel.connect()
@@ -45,7 +44,7 @@ async def play(ctx, url):
         voice_client.stop()
     
     try:
-        await ctx.send("Carregando...")
+        await context.send("Carregando...")
         
         # Baixa e prepara o áudio
         player = await YTDLSource.from_url(url, loop=bot.loop)
@@ -53,19 +52,19 @@ async def play(ctx, url):
         # Reproduz a música
         voice_client.play(player)
         
-        await ctx.send(f"Playing: **{player.title}**")
+        await context.send(f"Playing: **{player.title}**")
         
     except Exception as e:
-        await ctx.send(f"Erro: {str(e)}")
+        await context.send(f"Erro: {str(e)}")
 
 @bot.command(name='tocar')
-async def tocar(ctx, *, query):
-    if not ctx.author.voice:
-        await ctx.send ('Você precisa estar em um canal de voz')
+async def tocar(context, *, query):
+    if not context.author.voice:
+        await context.send ('Você precisa estar em um canal de voz')
         return
 
-    channel = ctx.author.voice.channel
-    voice_client = ctx.voice_client
+    channel = context.author.voice.channel
+    voice_client = context.voice_client
 
     if voice_client is None:
         voice_client = await channel.connect()
@@ -76,29 +75,29 @@ async def tocar(ctx, *, query):
         voice_client.stop()
 
     try:
-        await ctx.send("Buscando o fado, só um minuto...")
+        await context.send("Buscando o fado, só um minuto...")
 
         search_query = f'ytsearch:{query.strip()}'
         player = await YTDLSource.from_url(search_query, loop=bot.loop)
 
         voice_client.play(player)
-        await ctx.send(f"Coloquei pra tocar essa pedrada: **{player.title}**")
+        await context.send(f"Coloquei pra tocar essa pedrada: **{player.title}**")
 
     except Exception as e:
-        await ctx.send(f"Deu ruim!: {str(e)}")
+        await context.send(f"Deu ruim!: {str(e)}")
 
 @bot.command(name='stop')
-async def stop(ctx):
-    voice_client = ctx.voice_client
+async def stop(context):
+    voice_client = context.voice_client
 
     # Se o bot estiver tocando ele é parado e desconectado.
     if voice_client and voice_client.is_connected():
         if voice_client.is_playing():
             voice_client.stop()
         await voice_client.disconnect()
-        await ctx.send('Acabou a festa, então o bot saiu do canal')
+        await context.send('Acabou a festa, então o bot saiu do canal')
     else:
-        await ctx.send('O bot não está em um canal de voz')
+        await context.send('O bot não está em um canal de voz')
 
 if __name__ == "__main__":
     TOKEN = os.getenv('DISCORD_TOKEN')
