ccf2527b66d124c23b5c6e24306721e9342d1c67
Lucas Garcia Marcos <lucas.marcos@careintelligence.ai>
2025-07-19 00:55:54 -0300

    Feat: separacao source_client

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import discord
 import yt_dlp
-import asyncio
 import os
+from source_client import YTDLSource
 from discord.ext import commands
 from dotenv import load_dotenv
 
@@ -23,31 +23,10 @@ ytdl_format_options = {
     'no_warnings': True,
 }
 
-ffmpeg_options = {
-    'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5',
-    'options': '-vn'
-}
-
 ytdl = yt_dlp.YoutubeDL(ytdl_format_options)
 
 # URL exclusivo kasino
 KASINO = "https://youtu.be/LCDaw0QmQQc?si=s9E1Z9QNThLdL9qc"
-
-class YTDLSource(discord.PCMVolumeTransformer):
-    def __init__(self, source, *, data, volume=0.5):
-        super().__init__(source, volume)
-        self.data = data
-        self.title = data.get('title')
-
-    @classmethod
-    async def from_url(self, url, *, loop=None):
-        loop = loop or asyncio.get_event_loop()
-        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=False))
-        
-        if 'entries' in data:
-            data = data['entries'][0]
-        
-        return self(discord.FFmpegPCMAudio(data['url'], **ffmpeg_options), data=data)
 
 @bot.event
 async def on_ready():
---
+++ source_client.py
@@ -0,0 +1,23 @@
+import asyncio
+import discord
+
+ffmpeg_options = {
+    'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5',
+    'options': '-vn'
+}
+
+class YTDLSource(discord.PCMVolumeTransformer):
+    def __init__(self, source, *, data, volume=0.5):
+        super().__init__(source, volume)
+        self.data = data
+        self.title = data.get('title')
+
+    @classmethod
+    async def from_url(self, url, *, loop=None):
+        loop = loop or asyncio.get_event_loop()
+        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=False))
+        
+        if 'entries' in data:
+            data = data['entries'][0]
+        
+        return self(discord.FFmpegPCMAudio(data['url'], **ffmpeg_options), data=data)
